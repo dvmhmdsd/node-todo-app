c1093734ed841b892c4d39ae6386f5c88450cc5d
mohamedsaad4 <mahameds19999@gmail.com>
2019-01-02 23:23:48 +0200

    handle delete requests

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,16 +1,26 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'play game'}, {item: 'read a book'}];
+
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
     // handle get request
     app.get('/todo', function(req, res) {
-
+        res.render('todo', {todos: data});
     });
 
     // handle post requests
-    app.post('/todo', function(req, res) {
-
+    app.post('/todo', urlEncodedParser, function(req, res) {
+        data.push(req.body);
+        res.json(data); // to send it to the front-end
     });
 
     // handle delete requests
-    app.delete('/todo', function(req, res) {
-
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, "-") !== req.params.item; // we used replace method to match that on the ajax file
+        });
+        res.json(data);
     });
 };
